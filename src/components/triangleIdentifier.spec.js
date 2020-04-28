e9f5ac0cf2a78751961f9a2f3b9e74f639ce528b
Matheus Brondani <matheuso@trin.ca>
2020-04-28 03:05:35 -0300

    Update tests

--- src/components/triangleIdentifier.spec.js
+++ src/components/triangleIdentifier.spec.js
@@ -3,26 +3,21 @@ import { render } from "@testing-library/react";
 import TriangleIdentifier from "./triangleIdentifier";
 
 test("Test Scalene triangle", () => {
-  const { debug, getByText } = render(TriangleIdentifier(2, 3, 4));
-  debug();
-  const renderText = getByText("ESCALENO");
-  expect(renderText).toBeInTheDocument();
+  const { getByText } = render(TriangleIdentifier(2, 3, 4));
+  expect(getByText("ESCALENO")).toBeTruthy();
 });
 
 test("Test Equilateral triangle", () => {
   const { getByText } = render(TriangleIdentifier(5, 5, 5));
-  const renderText = getByText("EQUILATERO");
-  expect(renderText).toBeInTheDocument();
+  expect(getByText("EQUILATERO")).toBeTruthy();
 });
 
 test("Test Isosceles triangle", () => {
   const { getByText } = render(TriangleIdentifier(6, 6, 7));
-  const renderText = getByText("ISOSCELES");
-  expect(renderText).toBeInTheDocument();
+  expect(getByText("ISOSCELES")).toBeTruthy();
 });
 
 test("Test Invalid triangle", () => {
   const { getByText } = render(TriangleIdentifier(6, 6, -7));
-  const renderText = getByText("INVALIDO");
-  expect(renderText).toBeInTheDocument();
+  expect(getByText("INVALIDO")).toBeTruthy();
 });

07df02d9e3e3f08b7dd5db770720da7c721a83d2
Matheus Brondani <matheuso@trin.ca>
2020-05-04 19:58:59 -0300

    Update function validation

--- src/components/triangleIdentifier.js
+++ src/components/triangleIdentifier.js
@@ -2,12 +2,35 @@ import React from "react";
 import types from "../enum/types";
 
 function TriangleIdentifier(a, b, c) {
+  if (isNullOrUndefined(a, b, c)) return types.INVALID;
+
+  if (isInvalidNumber(a, b, c)) return types.INVALID;
+
   if (a < b + c && b < a + c && c < b + a) {
     if (a === b && b === c) return types.EQUILATERAL;
     if (a !== b && a !== c && b !== c) return types.SCALENE;
     return types.ISOSCELES;
   }
-  return types.INVALID;
+  return types.NOTTRIANGLE;
+}
+
+function isNullOrUndefined(a, b, c) {
+  if (a === undefined || a === null) return true;
+  if (b === undefined || b === null) return true;
+  if (c === undefined || c === null) return true;
+
+  return false;
+}
+
+function isInvalidNumber(a, b, c) {
+  const greaterThenZeroOrAllZero =
+    (a > 0 && b > 0 && c > 0) || (a === 0 && b === 0 && c === 0);
+  const isInteger =
+    Number.isInteger(a) && Number.isInteger(b) && Number.isInteger(c);
+
+  if (greaterThenZeroOrAllZero && isInteger) return false;
+
+  return true;
 }
 
 export default TriangleIdentifier;
